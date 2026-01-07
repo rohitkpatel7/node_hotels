295729919970e9b78655251d248fcd00c42d05f8
rohitkpatel7 <rkpatel07082003@gmail.com>
2026-01-07 11:07:58 +0530

    Clean up server.js by removing commented code
    Removed commented-out code for Express server setup and person route handling.

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-//   import express from 'express'
 
-// const app = express()
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World')
-// })
-
-// app.listen(3000, () => {
-//   console.log('Server is running on http://localhost:3000')
-// })
 
 const express = require("express");
 const app = express();
@@ -27,35 +17,6 @@ app.get("/", (req, res) => {
 });
 
          
-
-//not use in todays
-
-// app.post("/persons", (req, res) => {
-//   const data = req.body; // Assuming req.body contains the person data
-
-//   //create a new person document using the mongoose model
-//   const newPerson = new Person(data);
-
-//   //save the new person document to the database
-//   newPerson.save((error, savedperson) => {
-//     if(error) {
-//     console.log("Error saving person:", error);
-//     res.status(500).json({ error: "internal server error" });
-//     }
-//     else {
-//       console.log("Person saved successfully:");
-//       res.status(201).json(savedperson);
-//     }
-//   })
-// });
-
-
-
-
- 
-
-
-
 
 //import the router files
 const personRoutes = require('./routes/personRoutes');
